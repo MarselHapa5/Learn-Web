31588b08f5a552183e1d01da9f88df7d963b67f9
MarselHapa5 <marselreklama05@gmail.com>
2022-01-31 00:42:17 +0400

    java /0 test

--- infTest.js
+++
@@ -1,6 +0,0 @@
-let bar = 1
-if (bar=="1"){
-    var foo 
-    Number(foo) = 100*100+100-100/0
-    console.log("${foo}")
-}
---
+++ interestingThingsOnJs/infTest.js
@@ -0,0 +1,5 @@
+let bar = 1
+let baz = 100*100+100-100/0
+if (bar=="1"){
+    console.log(100/0)
+}
